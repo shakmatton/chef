d71b31a63b69a955f0494e25960fdc98ad7bcfe5
shakmatton <118413324+shakmatton@users.noreply.github.com>
2023-06-25 17:58:46 -0300

    updated main.js with pumpkin model

--- main.js
+++ main.js
@@ -21,7 +21,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const apple = await loadGLTF("./assets/maçã/Maçã.gltf")
         const pepper = await loadGLTF("./assets/pimenta/Pimenta.gltf")
-        const carrot = await loadGLTF ("./assets/cenoura/Cenoura.gltf")
+        const carrot = await loadGLTF("./assets/cenoura/Cenoura.gltf")
+        const pumpkin = await loadGLTF("./assets/abóbora/Abóbora.gltf")
 
         apple.scene.scale.set(0.05, 0.05, 0.05)
         apple.scene.position.set(-0.44, 0.09, 0)
@@ -32,24 +33,31 @@ document.addEventListener("DOMContentLoaded", () => {
         carrot.scene.scale.set(0.275, 0.275, 0.275)
         carrot.scene.position.set(-0.285, 0.425, 0)
 
+        pumpkin.scene.scale.set(0.25, 0.25, 0.25)
+        pumpkin.scene.position.set(0.404, 0.182, 0)
+        pumpkin.scene.rotation.set(0.6, 0.2, 0)
+
         const chefAnchor = mindarThree.addAnchor(0)
         
         chefAnchor.group.add(apple.scene)
         chefAnchor.group.add(pepper.scene)
         chefAnchor.group.add(carrot.scene)
-        
+        chefAnchor.group.add(pumpkin.scene)
         
         const appleMixer = new THREE.AnimationMixer(apple.scene)
         const pepperMixer = new THREE.AnimationMixer(pepper.scene)
         const carrotMixer = new THREE.AnimationMixer(carrot.scene)
+        const pumpkinMixer = new THREE.AnimationMixer(pumpkin.scene)
 
         const appleAction = appleMixer.clipAction(apple.animations[0])        
         const pepperAction = pepperMixer.clipAction(pepper.animations[0])
         const carrotAction = carrotMixer.clipAction(carrot.animations[0])
+        const pumpkinAction = pumpkinMixer.clipAction(pumpkin.animations[0])
         
         appleAction.play()
         pepperAction.play()
         carrotAction.play()
+        pumpkinAction.play()
 
        /* const appleMixer = new THREE.AnimationMixer(apple.scene)
         apple.animations.forEach((clip) => {
